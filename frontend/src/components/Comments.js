9d2337cf3bc90f5e82ff71d0bb0686bf882d9cfc
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2022-09-30 16:36:12 +0000

    feat: add comment

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import socketIO from "socket.io-client";
+
+const socket = socketIO.connect("http://localhost:4000");
+
 const Comments = () => {
-  return <div></div>;
+  const [comment, setComment] = useState("");
+  const addComment = (e) => {
+    e.preventDefault();
+    console.log({
+      comment,
+      userId: localStorage.getItem("userId"),
+    });
+
+    setComment("");
+  };
+
+  return (
+    <div className="comments__container">
+      <form className="comment__form" onSubmit={addComment}>
+        <label htmlFor="comment">Add comment</label>
+        <textarea
+          placeholder="Type your comment..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          rows={5}
+          id="comment"
+          name="comment"
+          required
+        ></textarea>
+        <button className="commentBtn">ADD COMMENT</button>
+      </form>
+
+      <div className="comments__section">
+        <h2>Existing Comments</h2>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
