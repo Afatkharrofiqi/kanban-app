c95c69e160008375fe52182beed7b157d41f6051
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2022-09-30 16:40:30 +0000

    feat(data): dummy

--- backend/index.js
+++ backend/index.js
@@ -25,10 +25,56 @@ socketIO.on("connection", (socket) => {
   });
 });
 
+//👇🏻 Generates a random string
+const fetchID = () => Math.random().toString(36).substring(2, 10);
+//👇🏻 Nested object
+let tasks = {
+  pending: {
+    title: "pending",
+    items: [
+      {
+        id: fetchID(),
+        title: "Send the Figma file to Dima",
+        comments: [],
+      },
+    ],
+  },
+  ongoing: {
+    title: "ongoing",
+    items: [
+      {
+        id: fetchID(),
+        title: "Review GitHub issues",
+        comments: [
+          {
+            name: "David",
+            text: "Ensure you review before merging",
+            id: fetchID(),
+          },
+        ],
+      },
+    ],
+  },
+  completed: {
+    title: "completed",
+    items: [
+      {
+        id: fetchID(),
+        title: "Create technical contents",
+        comments: [
+          {
+            name: "Dima",
+            text: "Make sure you check the requirements",
+            id: fetchID(),
+          },
+        ],
+      },
+    ],
+  },
+};
+
 app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello World",
-  });
+  res.json(tasks);
 });
 
 app.listen(PORT, () => {
