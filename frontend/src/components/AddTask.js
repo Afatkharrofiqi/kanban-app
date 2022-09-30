f126d100665fc874dfb407285e984dabab0e1b05
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2022-09-30 16:36:07 +0000

    feat: add task

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const AddTask = () => {
-  return <>Add Tasks</>;
+  const [task, setTask] = useState("");
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    // Logs the task to the console
+    console.log({ task });
+    setTask("");
+  };
+
+  return (
+    <form className="form__input" onSubmit={handleAddTodo}>
+      <label htmlFor="task">Add Todo</label>
+      <input
+        type="text"
+        name="task"
+        id="task"
+        value={task}
+        className="input"
+        required
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button className="addTodoBtn">ADD TODO</button>
+    </form>
+  );
 };
 
 export default AddTask;
